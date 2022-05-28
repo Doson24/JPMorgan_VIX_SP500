040acc1a37ba8c6d0e7e97cb5f010e6540b09011
Doson <karbushev-1991@mail.ru>
2022-05-28 12:23:08 +0700

    Complete

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+from pandas.tseries.offsets import DateOffset
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+vix_df = yf.download('^VIX')
 
+"""вычисление скользящей средней за 30 дней через оконную функцию rolling"""
+vix_df['MA'] = vix_df['Close'].rolling('30D').mean()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""фильтруем значения: цена закрытия выше на 50% МА"""
+vix_df_filter = vix_df[vix_df.Close > 1.5 * vix_df.MA]
 
+"""
+diff() разница значений 
+Переводим дни в число и фильтруем значения >= 30 
+"""
+boolean_mask = pd.Series(vix_df_filter.index).diff() / np.timedelta64(1, 'D') >= 30
+boolean_mask[0] = True
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+signals = vix_df_filter[boolean_mask.values]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+sp_df = yf.download('^GSPC', start='1990-01-01')
+
+test_ = sp_df[(sp_df.index >= signals.index[0]) &
+              (sp_df.index <= signals.index[0] + DateOffset(months=6))]
+
+a = (test_.Close.pct_change() + 1).prod()
+b = test_['Close'].pct_change().sum()
+
+returns = []
+for i in range(len(signals)):
+    subdf = sp_df[(sp_df.index >= signals.index[i]) &
+                  (sp_df.index <= signals.index[i] + DateOffset(months=6))]
+    returns.append(subdf['Close'].pct_change().sum() * 100)
+    # returns.append((subdf.Close.pct_change()+1).prod())
+# print(returns)
+pd.DataFrame(returns, index=signals.index.date).plot(kind='bar')
+"""Для полного отображения даты по оси Х"""
+plt.tight_layout()
+plt.show()
